50dd3d03428f728ed15faae03c717f5e948eb05e
vishwitha nandaKumar <vishwitha nandaKumar>
2020-04-13 19:45:49 +0530

    stack and linked list

--- linkedlist.py
+++ linkedlist.py
@@ -17,6 +17,29 @@ class LinkedList():
         else:
             newNode.nextNode = self.head
             self.head = newNode
+
+    def removeStart(self,data):
+        print("entered remove")
+        self.size -= 1
+        if self.head == None :
+            print("self.head")
+            return
+        else:
+            print("else")
+            currentNode = self.head
+            previousNode = None
+
+            while currentNode.data != data:
+                previousNode = currentNode
+                currentNode = currentNode.nextNode
+
+
+            if previousNode == None:
+                self.head = currentNode.nextNode
+            else:
+                previousNode.nextNode = currentNode.nextNode
+
+
     def insertEnd(self,data):
         self.size += 1
         newNode = Node(data)
@@ -28,16 +51,21 @@ class LinkedList():
                 actualNode = actualNode.nextNode
             actualNode.nextNode = newNode
 
-    def printList(self):
+    def traverseList(self):
         temp = self.head
         while (temp):
             print(temp.data)
             temp = temp.nextNode
 
+    def size_of_list(self):
+        return self.size
+
+
 if __name__ == '__main__':
     list1= LinkedList()
-    list1.insertStart(10)
-    list1.insertStart(12)
-    list1.printList()
-    list1.insertEnd(100)
-    list1.printList()
+
+    list1.insertStart(20)
+    list1.traverseList()
+    list1.removeStart(20)
+    list1.traverseList()
+    print(list1.size_of_list())
